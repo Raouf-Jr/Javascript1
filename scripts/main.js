42570bfdc4c933ca7365d11c5de5ecb7a0a59f4c
Raouf-Jr <abdourraouftogu@gmail.com>
2023-08-31 12:01:31 +0000

    Jajoute mon html et java script

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+let mots = document.getElementById("mots")
+console.log(mots)
+let h2 = document.querySelector("#zoneProposition h2")
+console.log(h2)
---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+for (let i = 0; i > 5; i++) {
+  console.log("Buzz l’éclair à la rescousse !")
+}
